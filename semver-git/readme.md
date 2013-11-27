b534de712fed540c27b8c734675ba2a008cfcaaa
Mikael Brannstrom <mikael.brannstrom@cinnober.com>
2013-11-27 10:46:28 +0100

    Initial version of semver-git

---
+++ semver-git/readme.md
@@ -0,0 +1,34 @@
+# semver-git #
+
+The gradle plugin 'semver-git' sets the `project.version` based on _annotated_ git tags.
+Version numbers must follow [Semantic Versioning 2.0.0](http://semver.org/spec/v2.0.0.html), with the syntax _major.minor.patch_.
+
+# Usage #
+
+In your `build.gradle` file:
+
+    buildscript {
+        repositories {
+            maven {
+                url uri("http://nexus.cinnober.com/nexus/content/repositories/buildtest_snapshots/")
+            }
+            maven {
+                url uri("http://nexus.cinnober.com/nexus/content/repositories/buildtest_releases/")
+            }
+        }
+        dependencies {
+            classpath group: 'com.cinnober.gradle', name: 'semver-git', version: '0.1.0-SNAPSHOT'
+        }
+    }
+    // optionally: ext.nextVersion = "major", "minor" (default), "patch" or e.g. "3.0.0-rc2"
+    apply plugin 'semver-git'
+
+Then everything should just work. To create a release, create an annotated git tag, e.g.:
+
+    git tag -a 1.0.0 -m "New release"
+    git push --tags
+
+When standing on an annotated tag commit, then version is simply the same as the tag (1.0.0 in this example).
+After a few commits `git describe` will show something like `1.0.0-5-g524234` in which case the version
+is the snapshot of the next version. In this example the next version is minor, and the version will be
+`1.1.0-SNAPSHOT`.
---
+++ semver-git/src/main/groovy/com/cinnober/gradle/semver/git/SemverGitPlugin.groovy
@@ -0,0 +1,98 @@
+package com.cinnober.gradle.semver.git
+
+import org.gradle.api.Project
+import org.gradle.api.Plugin
+
+class SemverGitPlugin implements Plugin<Project> {
+
+    def static String getGitVersion(String nextVersion) {
+        def proc = "git describe --exact-match".execute();
+        proc.waitFor();
+        if (proc.exitValue() == 0) {
+            return checkVersion(proc.text.trim());
+        }
+        proc = "git describe".execute();
+        proc.waitFor();
+        if (proc.exitValue() == 0) {
+            def version = (proc.text.trim() =~ /-[0-9]+-g[0-9a-f]+$/).replaceFirst("")
+            return getNextVersion(version, nextVersion);
+        }
+        return getNextVersion("0.0.0", nextVersion)
+    }
+
+    def static checkVersion(String version) {
+        parseVersion(version);
+        return version;
+    }
+
+    def static parseVersion(String version) {
+        def pattern = /([0-9]+)\.([0-9]+)\.([0-9]+)(-([a-zA-Z0-9.-]+))?/
+        def matcher = version =~ pattern
+        def arr = matcher.collect { it }[0]
+        if (arr == null) {
+            throw new IllegalArgumentException("Not a valid version: '" + version + "'")
+        }
+        return [arr[1].toInteger(), arr[2].toInteger(), arr[3].toInteger(), arr[5]]
+    }
+
+    def static formatVersion(version) {
+        return "" + version[0] + "." + version[1] + "." + version[2] + (version[3] != null ? "-" + version[3] : "");
+    }
+
+    def static String getNextVersion(String version, String nextVersion) {
+        def v
+        switch (nextVersion) {
+            case "major":
+                v = parseVersion(version)
+                if (v[3] == null) {
+                    v[0] += 1
+                    v[1] = 0
+                    v[2] = 0
+                }
+                v[3] = "SNAPSHOT"
+                return formatVersion(v)
+            case "minor":
+                v = parseVersion(version)
+                if (v[3] == null) {
+                    v[1] += 1
+                    v[2] = 0
+                }
+                v[3] = "SNAPSHOT"
+                return formatVersion(v);
+            case "patch":
+                v = parseVersion(version)
+                if (v[3] == null) {
+                    v[2] += 1
+                }
+                v[3] = "SNAPSHOT"
+                return formatVersion(v);
+            default:
+                return checkVersion(nextVersion);
+        }
+    }
+
+    void apply(Project project) {
+        def nextVersion = "minor"
+        if (project.ext.properties.containsKey("nextVersion")) {
+            nextVersion = project.ext.nextVersion
+        }
+        project.version = getGitVersion(nextVersion)
+        project.task('showVersion') << {
+            description "Show project.version"
+            println "Version: " + project.version
+        }
+    }
+}
+
+//println "parse (1.2.3) = " + SemverGitPlugin2.parseVersion("1.2.3")
+//println "format [1,2,3,null]=" + SemverGitPlugin2.formatVersion(SemverGitPlugin2.parseVersion("1.2.3"))
+//println "format [1,2,3,null]=" + SemverGitPlugin2.formatVersion([1,2,3,null])
+//println "format [1,2,3,rc1]=" + SemverGitPlugin2.formatVersion([1,2,3,"rc1"])
+//println "parse (1.2.3-rc2) = " + SemverGitPlugin2.parseVersion("1.2.3-rc2")
+//println "nextVer(1.2.3) = " + SemverGitPlugin2.getNextVersion("1.2.3", "major")
+//println "nextVer(1.2.3) = " + SemverGitPlugin2.getNextVersion("1.2.3", "minor")
+//println "nextVer(1.2.3) = " + SemverGitPlugin2.getNextVersion("1.2.3", "patch")
+//println "nextVer(1.2.3-rc2) = " + SemverGitPlugin2.getNextVersion("1.2.3-rc2", "major")
+//println "nextVer(1.2.3-rc2) = " + SemverGitPlugin2.getNextVersion("1.2.3-rc2", "minor")
+//println "nextVer(1.2.3-rc2) = " + SemverGitPlugin2.getNextVersion("1.2.3-rc2", "patch")
+//println "nextVer(1.2.3-rc2) = " + SemverGitPlugin2.getNextVersion("1.2.3-rc2", "4.5.6")
