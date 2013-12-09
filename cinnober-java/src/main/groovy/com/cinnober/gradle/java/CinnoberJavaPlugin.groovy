5c66d452e7239214928ac25e7dc621621de038e6
Mikael Brannstrom <mikael.brannstrom@cinnober.com>
2013-12-09 13:26:09 +0100

    Added xlint property. Usage: gradle -Pxlint=unchecked build

--- cinnober-java/src/main/groovy/com/cinnober/gradle/java/CinnoberJavaPlugin.groovy
+++ cinnober-java/src/main/groovy/com/cinnober/gradle/java/CinnoberJavaPlugin.groovy
@@ -3,6 +3,7 @@ package com.cinnober.gradle.java
 import org.gradle.api.Project
 import org.gradle.api.Plugin
 import org.gradle.api.tasks.bundling.Jar
+import org.gradle.api.tasks.compile.Compile
 
 class CinnoberJavaPlugin implements Plugin<Project> {
 
@@ -51,6 +52,14 @@ class CinnoberJavaPlugin implements Plugin<Project> {
         project.artifacts { 
             archives project.tasks.sourcesJar
             archives project.tasks.javadocJar
+        }
+
+        if (project.hasProperty('xlint')) {
+            project.property('xlint').split(',').each { value ->
+                project.tasks.withType(Compile) {
+                    options.compilerArgs << "-Xlint:" + value
+                }
+            }
         }
     }
 }
