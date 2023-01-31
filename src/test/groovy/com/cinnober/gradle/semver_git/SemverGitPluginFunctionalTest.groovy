9679d203e0ad43e6910f390e93af9258885dda8e
Emil Lundberg <emil@yubico.com>
2023-01-31 19:50:45 +0100

    Expand functional test

--- src/test/groovy/com/cinnober/gradle/semver_git/SemverGitPluginFunctionalTest.groovy
+++ src/test/groovy/com/cinnober/gradle/semver_git/SemverGitPluginFunctionalTest.groovy
@@ -59,7 +59,7 @@ class SemverGitPluginFunctionalTest extends Specification {
 
     def "Prefixed tag"() {
         when:
-        setupPluginWithClasspath('v')
+        setupPluginWithClasspath(prefix: 'v')
         init()
         commit()
         tag("v1.0.0")
@@ -71,7 +71,7 @@ class SemverGitPluginFunctionalTest extends Specification {
 
     def "Prefixed tag with commit"() {
         when:
-        setupPluginWithClasspath('v')
+        setupPluginWithClasspath(prefix: 'v')
         init()
         commit()
         tag("v1.0.0")
@@ -82,6 +82,112 @@ class SemverGitPluginFunctionalTest extends Specification {
         result.getOutput().contains("Version: 1.1.0-SNAPSHOT")
     }
 
+    def "Dirty tag with default config"() {
+        when:
+        setupPluginWithClasspath()
+        init()
+        commit()
+        tag("1.0.0")
+        touchBuildFile()
+        BuildResult result = gradleBuild("showVersion")
+
+        then:
+        result.getOutput().contains("Version: 1.0.0")
+    }
+
+    def "Dirty tag with snapshotSuffix with <dirty>"() {
+        when:
+        setupPluginWithClasspath(snapshotSuffix: "SNAPSHOT<dirty>")
+        init()
+        commit()
+        tag("1.0.0")
+        touchBuildFile()
+        BuildResult result = gradleBuild("showVersion")
+
+        then:
+        result.getOutput().contains("Version: 1.0.0")
+    }
+
+    def "Dirty untagged commit with default config"() {
+        when:
+        setupPluginWithClasspath()
+        init()
+        commit()
+        tag("1.0.0")
+        commit()
+        touchBuildFile()
+        BuildResult result = gradleBuild("showVersion")
+
+        then:
+        result.getOutput().contains("Version: 1.1.0-SNAPSHOT")
+    }
+
+    def "Dirty untagged commit with snapshotSuffix with <dirty>"() {
+        when:
+        setupPluginWithClasspath(snapshotSuffix: "SNAPSHOT<dirty>")
+        init()
+        commit()
+        tag("1.0.0")
+        commit()
+        touchBuildFile()
+        BuildResult result = gradleBuild("showVersion")
+
+        then:
+        result.getOutput().contains("Version: 1.1.0-SNAPSHOT-dirty")
+    }
+
+    def "Custom nextVersion"() {
+        when:
+        setupPluginWithClasspath(nextVersion: "major")
+        init()
+        commit()
+        tag("1.0.0")
+        commit()
+        BuildResult result = gradleBuild("showVersion")
+
+        then:
+        result.getOutput().contains("Version: 2.0.0-SNAPSHOT")
+    }
+
+    def "Custom snapshotSuffix"() {
+        when:
+        setupPluginWithClasspath(snapshotSuffix: "volatile")
+        init()
+        commit()
+        tag("1.0.0")
+        commit()
+        BuildResult result = gradleBuild("showVersion")
+
+        then:
+        result.getOutput().contains("Version: 1.1.0-volatile")
+    }
+
+    def "Custom dirtyMarker"() {
+        when:
+        setupPluginWithClasspath(snapshotSuffix: "SNAPSHOT<dirty>", dirtyMarker: "_MODIFIED")
+        init()
+        commit()
+        tag("1.0.0")
+        commit()
+        touchBuildFile()
+        BuildResult result = gradleBuild("showVersion")
+
+        then:
+        result.getOutput().contains("Version: 1.1.0-SNAPSHOT_MODIFIED")
+    }
+
+    def "Custom gitDescribeArgs"() {
+        when:
+        setupPluginWithClasspath(gitDescribeArgs: "--long")
+        init()
+        String shortCommit = commit()
+        tag("1.0.0")
+        BuildResult result = gradleBuild("showVersion")
+
+        then:
+        result.getOutput().contains("Version: 1.0.0-0-g${shortCommit}")
+    }
+
     def git(String argument) {
         def proc = ("git " + argument).execute(null, projectDir)
         proc.waitFor()
@@ -90,6 +196,7 @@ class SemverGitPluginFunctionalTest extends Specification {
 
     def init() {
         git("init")
+        git("add .")
 
         // Disable GPG signing in the test directory, otherwise user might be prompted for
         // manual intervention if they have this config set to true
@@ -97,29 +204,30 @@ class SemverGitPluginFunctionalTest extends Specification {
     }
 
     def commit() {
-        return git("commit --allow-empty -m empty")
+        git("commit --allow-empty -m empty")
+        return git("rev-parse --short HEAD").text.trim()
     }
 
     def tag(String name) {
         return git("tag -a $name -m tag")
     }
 
-    def setupPluginWithClasspath(
-            String prefix = null
-    ) {
-        build.write """
+    def touchBuildFile() {
+        build << "\n// foo\n"
+    }
+
+    def setupPluginWithClasspath(Map args = [:]) {
+        build << """
 plugins {
     id "com.cinnober.gradle.semver-git" apply false
 }
-// optionally: ext.nextVersion = "major", "minor" (default), "patch" or e.g. "3.0.0-rc2"
-// optionally: ext.snapshotSuffix = "SNAPSHOT" (default) or a pattern, e.g. "<count>.g<sha><dirty>-SNAPSHOT"
-// optionally: ext.dirtyMarker = "-dirty" (default) replaces <dirty> in snapshotSuffix
-// optionally: ext.gitDescribeArgs = '--match *[0-9].[0-9]*.[0-9]*' (default) or other arguments for git describe.
-"""
-        if (prefix) {
-            build << "ext.semverPrefix = '$prefix'"
-        }
-        build << """
+
+${args.nextVersion != null ? "ext.nextVersion = '${args.nextVersion}'" : ""}
+${args.snapshotSuffix != null ? "ext.snapshotSuffix = '${args.snapshotSuffix}'" : ""}
+${args.dirtyMarker != null ? "ext.dirtyMarker = '${args.dirtyMarker}'" : ""}
+${args.gitDescribeArgs != null ? "ext.gitDescribeArgs = '${args.gitDescribeArgs}'" : ""}
+${args.prefix != null ? "ext.semverPrefix = '${args.prefix}'" : ""}
+
 apply plugin: 'com.cinnober.gradle.semver-git'
 """
     }
